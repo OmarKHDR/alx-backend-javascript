5a0d9aa499a100fd54ad5e4c4b62c3f6e48fd335
OmarKHDR <omarsliman37@gmail.com>
2024-10-01 00:07:09 +0300

    random

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if(trueOrFalse) {
-	return [task, task2];
+    const task = true;
+    const task2 = false;
   }
-
   return [task, task2];
 }
