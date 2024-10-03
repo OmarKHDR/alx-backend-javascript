b47a0d42ce973f9c249c57c588ae06f4d426f3ac
OmarKHDR <omarsliman37@gmail.com>
2024-10-03 05:26:54 +0300

    last one async

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,11 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, filename) {
   const sign = signUpUser(firstName, lastName);
   const file = uploadPhoto(filename);
-  return Promise.allSettled([sign, file]);
+  return Promise.allSettled([sign, file])
+    .then((results) => {
+      results.map((result, index) => ({
+        status: result.status,
+        value: index === 0 ? result.value : result.reason,
+      }));
+    });
 }
