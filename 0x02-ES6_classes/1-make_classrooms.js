8a691772f5a2363b331267908d7ee2fbd3dcbb45
OmarKHDR <omarsliman37@gmail.com>
2024-10-06 19:41:13 +0300

    sec task class lst

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,12 @@
+import ClassRoom from "./0-classroom.js";
+
+// 19 20 34
+export default function initializeRooms() {
+  const ls = [];
+  ls[0] = new ClassRoom(19);
+  ls[1] = new ClassRoom(20);
+  ls[2] = new ClassRoom(34);
+  return ls;
+}
+
+console.log(initializeRooms())
