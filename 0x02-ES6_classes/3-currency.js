83a586916fc263fcfcb6664277bc01a9980b8a78
OmarKHDR <omarsliman37@gmail.com>
2024-10-06 21:37:00 +0300

    classes task getter and setter

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(cd) {
+    if (typeof cd !== 'string') {
+      throw new TypeError('code must be a character');
+    }
+    this._code = cd;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nm) {
+    if (typeof nm !== 'string') {
+      throw new TypeError('code must be a character');
+    }
+    this._name = nm;
+  }
+}
--- 0x02-ES6_classes/test.js
+++ 0x02-ES6_classes/test.js
@@ -4,3 +4,4 @@ const c = new HolbertonCourse("php", 13,['feq','few','vwe']);
 console.log(c.name ,typeof c.name , c.name === 'php')
 console.log(c.length ,typeof c.length , c.length === 13)
 console.log(c.students ,typeof c.students , c.students == ['feq','few','vwe'])
+
