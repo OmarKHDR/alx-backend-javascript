4ebcb823677aca240b5f9f61b03296bf6be99bad
OmarKHDR <omarsliman37@gmail.com>
2024-10-10 00:16:17 +0300

    classes task getter and setter 4

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 export default function createInt8TypedArray(len, pos, val) {
   if (pos - 1 <= len) {
     const bytes = DataView(new ArrayBuffer(len), 0, len);
-    bytes.setInt8(pos, val);
+    bytes.setInt8(pos - 1, val);
     return bytes;
   }
   throw new Error('Position outside range');
