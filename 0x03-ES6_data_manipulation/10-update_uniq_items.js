6a492349e3db184bbd48b4bfd48ed5ee12bfad47
OmarKHDR <omarsliman37@gmail.com>
2024-10-11 09:28:32 +0300

    world is cruel for real

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,9 +1,13 @@
 export default function updateUniqueItems(map) {
-  const key = map.keys();
-  for (const ele of key) {
-    if (map.get(ele) === 1) {
-      map.set(ele, 100);
+  try {
+    const key = map.keys();
+    for (const ele of key) {
+      if (map.get(ele) === 1) {
+        map.set(ele, 100);
+      }
     }
+  } catch (err) {
+    throw new Error('Cannot process');
   }
   return map;
 }
