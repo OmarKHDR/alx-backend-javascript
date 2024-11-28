80241fdb849f0b1de7ed37afbf6becc1e7702687
OmarKHDR <omarsliman37@gmail.com>
2024-11-29 00:25:13 +0200

    solved

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -11,29 +11,25 @@ function findDiffFields(arr) {
 }
 
 function countStudents(str) {
-  try {
-    fs.readFile(str, 'utf-8', (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      let rows = data.trim().split('\n');
-      for (let i = 0; i < rows.length; i += 1) {
-        rows[i] = rows[i].split(',');
-      }
-      rows = rows.slice(1);
-      const fields = findDiffFields(rows);
-      console.log('Number of students:', rows.length);
-      for (let i = 0; i < fields.length; i += 1) {
-        const fieldSt = rows.filter((item) => item[3] === fields[i]);
-        const namesList = fieldSt.map((item) => item[0]);
-        const names = namesList.join(', ');
-        const num = fieldSt.length;
-        console.log(`Number of students in ${fields[i]}: ${num}. List: ${names}`);
-      }
-    });
-  } catch {
-	throw new Error('Cannot load the database');
-  }
+  fs.readFile(str, 'utf-8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    let rows = data.trim().split('\n');
+    for (let i = 0; i < rows.length; i += 1) {
+      rows[i] = rows[i].split(',');
+    }
+    rows = rows.slice(1);
+    const fields = findDiffFields(rows);
+    console.log('Number of students:', rows.length);
+    for (let i = 0; i < fields.length; i += 1) {
+      const fieldSt = rows.filter((item) => item[3] === fields[i]);
+      const namesList = fieldSt.map((item) => item[0]);
+      const names = namesList.join(', ');
+      const num = fieldSt.length;
+      console.log(`Number of students in ${fields[i]}: ${num}. List: ${names}`);
+    }
+  });
 }
 
-countStudents('database.csv')
+module.exports = countStudents;
