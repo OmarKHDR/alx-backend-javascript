37c46d6419dfcf554bccd21225d98a16332d9f1c
OmarKHDR <omarsliman37@gmail.com>
2024-11-29 00:53:52 +0200

    solved

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,6 +5,9 @@ const app = require('node:http');
 module.exports = app;
 
 app.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
+  const responseText = 'Hello Holberton School!';
+  response.setHeader('Content-Type', 'text/plain');
+  response.setHeader('Content-Length', responseText.length);
+  response.statusCode = 200;
+  response.write(Buffer.from(responseText));
 }).listen(1245);
