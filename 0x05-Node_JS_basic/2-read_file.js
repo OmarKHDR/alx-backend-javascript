9c3fa60928d815d9a0d334c547962cb0c5aa7ca1
OmarKHDR <omarsliman37@gmail.com>
2024-11-29 00:20:58 +0200

    solved

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,30 +4,36 @@ const fs = require('fs');
 
 function findDiffFields(arr) {
   const field = new Set();
-  for (const item in arr) {
-    field.add(item[3]);
+  for (let i = 0; i < arr.length; i += 1) {
+    field.add(arr[i][3]);
   }
   return [...field];
 }
 
-module.exports = function countStudents(str) {
-  fs.readFile(str, 'utf-8', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    let rows = data.trim().split('\n');
-    for (let i = 0; i < rows.length; i += 1) {
-      rows[i] = rows[i].split(',');
-    }
-    rows = rows.slice(1);
-    const fields = findDiffFields(rows);
-    console.log('Number of students:', rows.length);
-    for (const field in fields) {
-      const fieldSt = rows.filter((item) => item[3] === field);
-      const namesList = fieldSt.map((item) => item[0]);
-      const names = namesList.join(', ');
-      const num = fieldSt.length;
-      console.log(`Number of students in ${field}: ${num}. List: ${names}`);
-    }
-  });
+function countStudents(str) {
+  try {
+    fs.readFile(str, 'utf-8', (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      let rows = data.trim().split('\n');
+      for (let i = 0; i < rows.length; i += 1) {
+        rows[i] = rows[i].split(',');
+      }
+      rows = rows.slice(1);
+      const fields = findDiffFields(rows);
+      console.log('Number of students:', rows.length);
+      for (let i = 0; i < fields.length; i += 1) {
+        const fieldSt = rows.filter((item) => item[3] === fields[i]);
+        const namesList = fieldSt.map((item) => item[0]);
+        const names = namesList.join(', ');
+        const num = fieldSt.length;
+        console.log(`Number of students in ${fields[i]}: ${num}. List: ${names}`);
+      }
+    });
+  } catch {
+	throw new Error('Cannot load the database');
+  }
 }
+
+countStudents('database.csv')
