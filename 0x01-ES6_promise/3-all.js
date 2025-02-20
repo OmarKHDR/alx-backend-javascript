76814c009b35fe61958202bccd7a42c8fe1464b7
OmarKHDR <omarsliman37@gmail.com>
2025-02-20 16:38:50 +0200

    hello 111

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,14 @@
 import { uploadPhoto, createUser } from './utlis';
 
-export default async function handleProfileSignup() {
-  try {
-    const name = await createUser();
-    const photo = await uploadPhoto();
-    console.log(`${photo.body} ${name.firstName} ${name.lastName}`);
-  } catch (err) {
-    console.log('Signup system offline');
+export default function handleProfileSignup() {
+  async function hello() {
+    try {
+      const name = await createUser();
+      const photo = await uploadPhoto();
+      console.log(`${photo.body} ${name.firstName} ${name.lastName}`);
+    } catch (err) {
+      console.log('Signup system offline');
+    }
   }
+  hello();
 }
